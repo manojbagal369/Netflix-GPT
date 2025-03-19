c42af4dcadca7a2220c6f96a5e78d6db4932e341
Manoj Bagal <manojbagal369@gmail.com>
2025-03-19 23:34:59 +0530

    Migrated create react to vite

---
+++ README.md
@@ -0,0 +1,12 @@
+# Netflix GPT
+
+-Vite app
+-Configured TailwindCSS
+
+#Features
+-Browse
+-Header
+-Main Movie
+-Trailer in Background
+-Title & Description
+-Movie Suggestion
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-500 underline">
+        Tailwind is Working! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
